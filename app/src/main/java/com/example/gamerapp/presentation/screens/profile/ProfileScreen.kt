a887abdd4392c5d2ea3f2e8cb8b75d2dc16bc0b4
enrique <daniellara5a2015@gmail.com>
2023-09-06 16:07:17 -0400

    [ADD] diseño de la pantalla de profile

--- app/src/main/java/com/example/gamerapp/presentation/screens/profile/ProfileScreen.kt
+++ app/src/main/java/com/example/gamerapp/presentation/screens/profile/ProfileScreen.kt
@@ -4,30 +4,18 @@ import android.annotation.SuppressLint
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
-import com.example.gamerapp.presentation.components.DefaultButtom
-import com.example.gamerapp.presentation.navigation.AppScreen
+import com.example.gamerapp.presentation.screens.profile.components.ProfileContent
 
 @OptIn(ExperimentalMaterial3Api::class)
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun ProfileScreen(navController: NavHostController, viewModel: ProfileViewModel = hiltViewModel()) {
-
     Scaffold(
         topBar = {},
         content = {
-                  DefaultButtom(
-                      modifier = Modifier,
-                      text = "Cerrrar Seccion",
-                      onClick = {
-                          viewModel.logout()
-                          navController.navigate(route = AppScreen.Login.route) {
-                              popUpTo(AppScreen.Profile.route) { inclusive = true }
-                          }
-                      }
-                  )
+            ProfileContent(navController)
         },
         bottomBar = {}
     )
---
+++ app/src/main/java/com/example/gamerapp/presentation/screens/profile/components/ProfileContent.kt
@@ -0,0 +1,105 @@
+package com.example.gamerapp.presentation.screens.profile.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavHostController
+import com.example.gamerapp.R
+import com.example.gamerapp.presentation.components.DefaultButtom
+import com.example.gamerapp.presentation.navigation.AppScreen
+import com.example.gamerapp.presentation.screens.profile.ProfileViewModel
+import com.example.gamerapp.presentation.ui.theme.GamerAppTheme
+
+
+@Composable
+fun ProfileContent(navController: NavHostController, viewModel: ProfileViewModel = hiltViewModel()) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+        Box {
+            Image(
+                modifier = Modifier.fillMaxWidth(),
+                painter = painterResource(id = R.drawable.background),
+                contentDescription = "imagen controles",
+                contentScale = ContentScale.Crop,
+                alpha = 0.6f
+            )
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Spacer(modifier = Modifier.height(30.dp))
+                Text(
+                    text = "Bienvenido",
+                    fontSize = 30.sp,
+                    fontWeight = FontWeight.Bold
+                )
+                Spacer(modifier = Modifier.height(55.dp))
+                Image(
+                    modifier = Modifier.size(115.dp),
+                    painter = painterResource(id = R.drawable.user),
+                    contentDescription = "imagen de usario"
+                )
+            }
+        }
+        Spacer(modifier = Modifier.height(55.dp))
+        Text(
+            text = "master",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            fontStyle = FontStyle.Italic
+        )
+        Text(
+            text = "user@gmail.com",
+            fontSize = 20.sp,
+            fontStyle = FontStyle.Italic
+        )
+        Spacer(modifier = Modifier.height(20.dp))
+        DefaultButtom(
+            modifier = Modifier.width(250.dp),
+            text = "Editar datos",
+            color = Color.White,
+            icon = Icons.Default.Edit,
+            onClick = {
+
+            }
+        )
+        Spacer(modifier = Modifier.height(10.dp))
+        DefaultButtom(
+            modifier = Modifier.width(250.dp),
+            text = "Cerrrar Seccion",
+            icon = Icons.Default.Close,
+            onClick = {
+                viewModel.logout()
+                navController.navigate(route = AppScreen.Login.route) {
+                    popUpTo(AppScreen.Profile.route) { inclusive = true }
+                }
+            }
+        )
+    }
+}
